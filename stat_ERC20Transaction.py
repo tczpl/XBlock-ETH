49fc57c8c0ce6b7efafa6d96425ae0399961d549
Peilin Zheng <tczpl@163.com>
2021-04-03 15:48:46 +0800

    Add files via upload

--- stat_ERC20Transaction.py
+++ stat_ERC20Transaction.py
@@ -2,52 +2,52 @@ import zipfile
 
 fileDir = "./"
 files = [
-	"0to999999_ERC20Transaction",
-	"1000000to1999999_ERC20Transaction",
-	"2000000to2999999_ERC20Transaction",
-	"3000000to3999999_ERC20Transaction",
-	"4000000to4999999_ERC20Transaction",
-	"5000000to5999999_ERC20Transaction",
-	"6000000to6999999_ERC20Transaction",
-	"7000000to7999999_ERC20Transaction",
-	"8000000to8999999_ERC20Transaction",
-	"9000000to9999999_ERC20Transaction",
-	"10000000to10999999_ERC20Transaction",
-	"11000000to11999999_ERC20Transaction"
+    "0to999999_ERC20Transaction",
+    "1000000to1999999_ERC20Transaction",
+    "2000000to2999999_ERC20Transaction",
+    "3000000to3999999_ERC20Transaction",
+    "4000000to4999999_ERC20Transaction",
+    "5000000to5999999_ERC20Transaction",
+    "6000000to6999999_ERC20Transaction",
+    "7000000to7999999_ERC20Transaction",
+    "8000000to8999999_ERC20Transaction",
+    "9000000to9999999_ERC20Transaction",
+    "10000000to10999999_ERC20Transaction",
+    "11000000to11999999_ERC20Transaction"
 ]
 
 tx_count = 0
 tokens = {}
 
 for file in files:
-	print(file)
-	theZIP = zipfile.ZipFile(fileDir+file+".zip", 'r')
-	theCSV = theZIP.open(file+".csv")
-	head = theCSV.readline()
+    print(file)
+    theZIP = zipfile.ZipFile(fileDir+file+".zip", 'r')
+    theCSV = theZIP.open(file+".csv")
+    head = theCSV.readline()
 
-	oneLine = theCSV.readline().decode("utf-8").strip()
-	while (oneLine!=""):
-		oneArray = oneLine.split(",")
+    oneLine = theCSV.readline().decode("utf-8").strip()
+    while (oneLine!=""):
+        oneArray = oneLine.split(",")
 
-		# blockNumber,timestamp,transactionHash,tokenAddress,from,to,fromIsContract,toIsContract,value
-		blockNumber			= int(oneArray[0])
-		timestamp			= int(oneArray[1])
-		transactionHash		= oneArray[2]
-		tokenAddress 		= oneArray[3]
-		sender		 		= oneArray[4]
-		to		 			= oneArray[5]
-		fromIsContract		= int(oneArray[6])
-		toIsContract		= int(oneArray[7])
-		value				= int(oneArray[8])
+        # blockNumber,timestamp,transactionHash,tokenAddress,from,to,fromIsContract,toIsContract,value
+        blockNumber     = int(oneArray[0])
+        timestamp       = int(oneArray[1])
+        transactionHash = oneArray[2]
+        tokenAddress    = oneArray[3]
+        sender          = oneArray[4]
+        to              = oneArray[5]
+        fromIsContract  = int(oneArray[6])
+        toIsContract    = int(oneArray[7])
+        value           = int(oneArray[8])
 
-		tx_count += 1
-		tokens[tokenAddress] = True
-		if tx_count % 100000 == 0:
-			print(tx_count, len(tokens))
-		oneLine = theCSV.readline().decode("utf-8").strip()
+        tx_count += 1
+        tokens[tokenAddress] = True
+        if tx_count % 100000 == 0:
+            print(tx_count, len(tokens))
+        oneLine = theCSV.readline().decode("utf-8").strip()
 
-	theCSV.close()
-	theZIP.close()
+    theCSV.close()
+    theZIP.close()
 
 print(tx_count, len(tokens))
 # 601955094 246609
--- stat_TokenInfo.py
+++ stat_TokenInfo.py
@@ -2,12 +2,12 @@ import zipfile
 
 fileDir = "./"	
 files = [
-	"ERC20TokenInfo",
-	"ERC721TokenInfo"
+    "ERC20TokenInfo",
+    "ERC721TokenInfo"
 ]	
 
 def ToInt(str):
-	return None if str=="None" else int(str)
+    return None if str=="None" else int(str)
 
 line_count = 0
 
@@ -19,18 +19,18 @@ head = theCSV.readline()
 
 oneLine = theCSV.readline().decode("utf-8").strip()
 while (oneLine!=""):
-	oneArray = oneLine.split(",")	
+    oneArray = oneLine.split(",")	
 
-	# address,name,symbol,totalSupply,decimal
-	address	 = oneArray[0]	
-	name		= oneArray[1]	
-	symbol	  = oneArray[2]	
-	totalSupply = ToInt(oneArray[3])	
-	decimal	 = ToInt(oneArray[4])	
+    # address,name,symbol,totalSupply,decimal
+    address     = oneArray[0]	
+    name        = oneArray[1]	
+    symbol      = oneArray[2]	
+    totalSupply = ToInt(oneArray[3])	
+    decimal     = ToInt(oneArray[4])	
 
-	print(address, name)
-	line_count += 1	
-	oneLine = theCSV.readline().decode("utf-8").strip()	
+    print(address, name)
+    line_count += 1	
+    oneLine = theCSV.readline().decode("utf-8").strip()	
   
 theCSV.close()
 theZIP.close()	
@@ -44,17 +44,17 @@ head = theCSV.readline()
 
 oneLine = theCSV.readline().decode("utf-8").strip()	
 while (oneLine!=""):
-	oneArray = oneLine.split(",")	
+    oneArray = oneLine.split(",")	
 
-	# address,name,symbol,totalSupply
-	address	 = oneArray[0]
-	name		= oneArray[1]
-	symbol	  = oneArray[2]
-	totalSupply = ToInt(oneArray[3])
+    # address,name,symbol,totalSupply
+    address     = oneArray[0]
+    name        = oneArray[1]
+    symbol      = oneArray[2]
+    totalSupply = ToInt(oneArray[3])
 
-	print(address, name)
-	line_count += 1	
-	oneLine = theCSV.readline().decode("utf-8").strip()	
+    print(address, name)
+    line_count += 1	
+    oneLine = theCSV.readline().decode("utf-8").strip()	
 
 theCSV.close()	
 theZIP.close()
